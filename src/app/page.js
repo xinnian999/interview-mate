6ba4e1f4c407623fb02352ac5236172debe70a4c
huyilin <v_huyilin@baidu.com>
2025-07-11 11:43:17 +0800

    init

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,96 @@
-import Image from "next/image";
+'use client';
+import {
+    XRequest,
+    Conversations,
+    Sender,
+    Welcome,
+    useXAgent,
+} from '@ant-design/x';
+import { Button, message } from 'antd';
+import { PlusOutlined } from '@ant-design/icons';
+import { useState } from 'react';
+
+const items = Array.from({ length: 4 }).map((_, index) => ({
+    key: `item${index + 1}`,
+    label: `Conversation Item ${index + 1}`,
+    disabled: index === 3,
+}));
+
+const BASE_URL = 'https://api.deepseek.com';
+const PATH = '';
+const MODEL = 'deepseek-chat';
+const API_KEY = 'Bearer sk-e5d58b69a0fa43d089a6f0152c06b326';
+
+const exampleRequest = XRequest({
+    baseURL: BASE_URL + PATH,
+    model: MODEL,
+    dangerouslyApiKey: API_KEY,
+});
 
 export default function Home() {
-  return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
+    const [loading, setLoading] = useState(false);
+    const [value, setValue] = useState('');
 
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    async function request() {
+        await exampleRequest.create(
+            {
+                messages: [{ role: 'user', content: value }],
+                stream: true,
+                agentId: 111,
+            },
+            {
+                onSuccess: (messages) => {
+                    console.log('onSuccess', messages);
+                },
+                onError: (error) => {
+                    console.error('onError', error);
+                },
+                onUpdate: (msg) => {
+                    console.log('onUpdate', msg);
+                },
+            }
+        );
+    }
+    return (
+        <div className='w-full h-dvh overflow-hidden flex'>
+            <div className='w-1/5 border-r border-gray-200 px-4 flex flex-col gap-3'>
+                <div className='p-4'>
+                    <span className='text-2xl'>面小助</span>
+                </div>
+
+                <Button type='primary' block icon={<PlusOutlined />}>
+                    新的面试
+                </Button>
+
+                <Conversations items={items} defaultActiveKey='item1' />
+            </div>
+
+            <div className='w-4/5 h-dvh overflow-hidden bg-gray-50 p-4 flex flex-col'>
+                <div className='flex-1'>
+                    <Welcome
+                        icon='https://mdn.alipayobjects.com/huamei_iwk9zp/afts/img/A*s5sNRo5LjfQAAAAAAAAAAAAADgCCAQ/fmt.webp'
+                        title="Hello, I'm Ant Design X"
+                        description='Base on Ant Design, AGI product interface solution, create a better intelligent vision~'
+                    />
+                </div>
+                <Sender
+                    loading={loading}
+                    value={value}
+                    onChange={(v) => {
+                        setValue(v);
+                    }}
+                    onSubmit={() => {
+                        setValue('');
+                        setLoading(true);
+                        request();
+                    }}
+                    onCancel={() => {
+                        setLoading(false);
+                        message.error('Cancel sending!');
+                    }}
+                    autoSize={{ minRows: 2, maxRows: 6 }}
+                />
+            </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
-  );
+    );
 }
